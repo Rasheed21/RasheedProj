01506c0182bcf15e527370fd74262511ea421135
Rasheed21 <rasheedrsa@yahoo.com>
2017-01-26 15:48:00 -0500

    Added a button with a counter

--- app/src/main/java/bo/rasheedproj/MainActivity.java
+++ app/src/main/java/bo/rasheedproj/MainActivity.java
@@ -2,12 +2,20 @@ package bo.rasheedproj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mOnclick(View view) {
+
+        ((Button)view).setText("Click"+(++count));
     }
 }
